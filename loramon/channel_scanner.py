19b81cabbc1161d5e2b3dad9e693e3ff0b204861
Alligitor <8754766+alligitor@users.noreply.github.com>
2025-04-10 09:33:16 -0400

    Removed Coding rate loop. Made it back to printing a line for every time LoraMon is launched.

--- loramon/channel_scanner.py
+++ loramon/channel_scanner.py
@@ -8,27 +8,29 @@ freq_end   = 928000000
 freq_step  =    125000
 
 spreading_factor_table = [7, 8, 9, 10, 11, 12]
-coding_rate_tabel = [5, 8]
+bandwidth_table = [125000, 250000]
+#pass a coding rate, although it doesn't really matter since LoraMon is RX only
+coding_rate = 5
 
 scan_duration = 2
 
+
 #print header
-print(" Frequency, Bandwidth, Spread Factor, Coding Rate, Duration, Captured Packets")
+print(" Frequency, Bandwidth, Spread Factor, Duration, Captured Packets")
 
 for freq in range(freq_start, freq_end, freq_step):
-    print(f"{freq}")
-    for sf in spreading_factor_table:
-        for cr in coding_rate_tabel:
+    for bw in bandwidth_table:
+        for sf in spreading_factor_table:
             command = ['python3', 'loramon.py']
             command.append(serial_port)
             command.append("--freq")
             command.append(str(freq))
             command.append("--bw")
-            command.append(str(freq_step))
+            command.append(str(bw))
             command.append("--sf")
             command.append(str(sf))
             command.append("--cr")
-            command.append(str(cr))
+            command.append(str(coding_rate))
             command.append("-C")
             command.append("--duration")
             command.append(str(scan_duration))
@@ -39,15 +41,11 @@ for freq in range(freq_start, freq_end, freq_step):
                 #Uncomment this to see the command we run
                 #print(f"{command}")
                 result = subprocess.run(command)
-                if result.returncode > 0:
-                    print()
-                    print("{:>{}} {:>{}} {:>{}} {:>{}} {:>{}} {:>{}}".format(
-                                                    freq, 10, 
-                                                    freq_step, 10, 
-                                                    sf, 14, 
-                                                    cr, 12, 
-                                                    scan_duration, 9, 
-                                                    result.returncode, 14))
-                #print(f"{freq}, {freq_step}, {sf}, {cr}, {scan_duration}, {result.returncode}")
+                print("{:>{}} {:>{}} {:>{}} {:>{}} {:>{}}".format(
+                                                freq, 10, 
+                                                bw, 10, 
+                                                sf, 14, 
+                                                scan_duration, 9, 
+                                                result.returncode, 14))
             except subprocess.CalledProcessError as e:
                 print(f"Failed to launch {command} with error {e}")
